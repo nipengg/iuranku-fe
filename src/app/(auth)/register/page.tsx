527937426a6511ac6a26632d444a8dddb94213c0
Neville Cornelius Tjampan <neville.tjampan@binus.edu>
2024-03-21 17:49:25 +0700

    fix: add suspense

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,6 +3,7 @@ import { useSearchParams } from 'next/navigation';
 import React, { useEffect } from 'react'
 import RegisterGoogle from './RegisterGoogle';
 import RegisterRegular from './RegisterRegular';
+import { Suspense } from 'react'
 
 const Register = () => {
   const searchParams = useSearchParams();
@@ -10,16 +11,18 @@ const Register = () => {
 
   return (
     <>
-      {
-        registerGoogle ?
-          <>
-            <RegisterGoogle />
-          </>
-          :
-          <>
-            <RegisterRegular />
-          </>
-      }
+      <Suspense>
+        {
+          registerGoogle ?
+            <>
+              <RegisterGoogle />
+            </>
+            :
+            <>
+              <RegisterRegular />
+            </>
+        }
+      </Suspense>
     </>
   )
 }
