f1dc6b746e5dbed96ce8312eeceb932af3851b66
Neville Cornelius Tjampan <neville.tjampan@binus.edu>
2024-04-15 18:05:37 +0700

    build: add group page

---
+++ src/app/(pages)/(client)/group/layout.tsx
@@ -0,0 +1,14 @@
+'use client';
+import Navbar from "@/components/Navbar/Navbar";
+
+export default function GroupLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <>
+            {children}
+        </>
+    )
+}
