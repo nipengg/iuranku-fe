705ee78dba00dfcbfd0d72e73cb058be9dccd93d
Repulsive31 <91827275+Repulsive31@users.noreply.github.com>
2024-11-07 21:05:57 +0700

    push register

--- src/app/(pages)/(auth)/register/page.tsx
+++ src/app/(pages)/(auth)/register/page.tsx
@@ -1,27 +1,22 @@
-'use client';
-import { useSearchParams } from 'next/navigation';
-import React, { useEffect } from 'react'
-import RegisterGoogle from '@/components/Register/RegisterGoogle';
-import RegisterRegular from '@/components/Register/RegisterRegular';
+"use client";
+import { useSearchParams } from "next/navigation";
+import React, { useEffect } from "react";
+import RegisterGoogle from "@/components/Register/RegisterGoogle";
+import RegisterRegular from "@/components/Register/RegisterRegular";
 
 const Register = () => {
-  const searchParams = useSearchParams();
-  const registerGoogle = searchParams.get('google');
+    const searchParams = useSearchParams();
+    const registerGoogle = searchParams.get("google");
 
-  return (
-    <>
-      {
-        registerGoogle ?
-          <>
-            <RegisterGoogle />
-          </>
-          :
-          <>
-            <RegisterRegular />
-          </>
-      }
-    </>
-  )
-}
+    return (
+        <>
+            {registerGoogle ? (
+                <>{<RegisterGoogle />}</>
+            ) : (
+                <>{<RegisterRegular />}</>
+            )}
+        </>
+    );
+};
 
-export default Register
+export default Register;
