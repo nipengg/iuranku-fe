f10979dc5035b288bc38eb14fedbf279bd4b7c13
Repulsive31 <91827275+Repulsive31@users.noreply.github.com>
2024-12-01 18:15:29 +0700

    add: announcement page + news page + group card list

--- src/app/(pages)/(client)/pengelola/payment/page.tsx
+++ src/app/(pages)/(client)/pengelola/payment/page.tsx
@@ -1,6 +1,6 @@
-import PaymentTable from "@/components/paymentTable";
+import PaymentTable from "@/components/PaymentTable";
 
-export default function paymentPage() {
+export default function PaymentPage() {
     return (
         <div>
             <PaymentTable />
