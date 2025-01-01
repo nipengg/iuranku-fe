21d83ad25f3ce4badb6891194adba6c4f145f353
nipengg <nevillecorneliustj@gmail.com>
2025-01-01 23:58:28 +0700

    build: image proxy loading indicator

--- src/components/Modal/FilePreviewModal.tsx
+++ src/components/Modal/FilePreviewModal.tsx
@@ -1,5 +1,5 @@
 import { BACKEND_STORAGE } from "@/constant";
-import React from "react";
+import React, { useState } from "react";
 
 interface FilePreviewModalProps {
     isModalOpen: boolean;
@@ -10,19 +10,29 @@ interface FilePreviewModalProps {
 const FilePreviewModal: React.FC<FilePreviewModalProps> = ({
     isModalOpen,
     onClose,
-    url
+    url,
 }) => {
+    const [isLoading, setIsLoading] = useState(true);
 
     if (!isModalOpen) return null;
+
     return (
         <div className="modal modal-open">
             <div className="modal-box">
                 <h2 className="text-xl font-bold mb-4">File Preview</h2>
-                <img
-                    src={`/api/proxyImage?url=${encodeURIComponent(BACKEND_STORAGE + url)}`}
-                    alt="Image Preview"
-                    className="max-w-full h-auto rounded-md mt-4"
-                />
+                <div className="flex justify-center items-center">
+                    {isLoading && <p>Loading...</p>}
+                    <img
+                        src={`/api/proxyImage?url=${encodeURIComponent(
+                            BACKEND_STORAGE + url
+                        )}`}
+                        alt="Image Preview"
+                        className={`max-w-full h-auto rounded-md mt-4 ${isLoading ? "hidden" : "block"
+                            }`}
+                        onLoad={() => setIsLoading(false)}
+                        onError={() => setIsLoading(false)}
+                    />
+                </div>
                 <div className="modal-action">
                     <button className="btn btn-ghost" onClick={onClose}>
                         Close
