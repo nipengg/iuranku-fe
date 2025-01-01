eb948d1e9dbf4e9b55e925ee6ca5888491b5579d
nipengg <nevillecorneliustj@gmail.com>
2025-01-01 22:55:17 +0700

    build: commit button disabled, proxy image from be

--- src/model/redux/Tuition.ts
+++ src/model/redux/Tuition.ts
@@ -3,11 +3,13 @@ import { BaseState } from "./BaseState";
 // State Interfaces
 export interface TuitionState extends BaseState {
     isLoadingMemberDetail: boolean;
+    isLoadingInsertTuition: boolean;
 }
 
 // Init
 export const TuitionStateInitial: TuitionState = {
     isLoading: false,
     isLoadingMemberDetail: false,
+    isLoadingInsertTuition: false,
     isError: false,
 }
